73e6a973e6a427668044566a9ea4296cc6f06dc9
Sungjun Lee <253eosam@gmail.com>
2024-09-30 16:59:42 +0900

    릴리즈노트 자동 작성 완료

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        현재 프로젝트 버전: {import.meta.env.PACKAGE_VERSION}
       </p>
     </>
   )
